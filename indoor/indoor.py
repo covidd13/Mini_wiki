cc48149acd1b2748c3b5261d493734c32750a89d
Raman Kukreti <ramankukreti00@gmail.com>
2023-04-29 06:14:30 +0000

    Sat, Apr 29, 2023, 11:44 AM +05:30

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -2,7 +2,7 @@ var1=str(input("Please Enter the string you want to convert"))
 var2=''
 for char in var1:
     if ord(char)>=65 and ord(char)<=90:
-        ord(char)=ord(char)+32
+        ord(char)==ord(char)+32
     else:
         pass
     var3=char
