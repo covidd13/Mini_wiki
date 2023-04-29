1671744f5e019e3b5b5ecbaa828f7e060f55da3c
Raman Kukreti <ramankukreti00@gmail.com>
2023-04-29 06:28:16 +0000

    Sat, Apr 29, 2023, 11:58 AM +05:30

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -3,9 +3,10 @@ var2=''
 for char in var1:
     if ord(char)>=65 and ord(char)<=90:
         cr=ord(char)+32
+        var3=chr(cr)
     else:
         var2+=char
-    var3=chr(cr)
+
     var2+=var3
 print(var2)
 
