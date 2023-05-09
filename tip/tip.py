4326aa09857b1b786ddf32329724ac1af02c810b
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-09 10:05:36 +0000

    Tue, May 9, 2023, 3:35 PM +05:30

--- tip/tip.py
+++ tip/tip.py
@@ -1,4 +1,4 @@
-def main():
+'''def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
@@ -16,4 +16,4 @@ def percent_to_float(p):
 
 
 
-main()
+main()'''
