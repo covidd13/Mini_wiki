3047f3a4c23b7900a96276f7473c405b1f1ea96f
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-09 12:44:57 +0000

    Tue, May 9, 2023, 6:14 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 ans=str(input("What is the to the great question of life,the universe and everything?"))
-if ans ==42 or "forty two" or "forty_two":
+if ans ==42 or ans== "forty two" or ans=="forty_two":
     print("Yes")
 else:
     print("No")
