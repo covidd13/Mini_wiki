0802a15bce28e5a129ba59d36e52af35df4f5a56
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-09 13:06:53 +0000

    Tue, May 9, 2023, 6:36 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 ans=str(input("What is the to the great question of life,the universe and everything?"))
-if ans == "42" or ans.casefold()== "forty two" or ans.casefold()=="forty-two":
+if ans == int(42) or ans.casefold()== "forty two" or ans.casefold()=="forty-two":
     print("Yes")
 else:
     print("No")
