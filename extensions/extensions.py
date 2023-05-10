eef9803cd0d67cbbe63cf098271e7e189c786a64
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-10 09:09:53 +0000

    Wed, May 10, 2023, 2:39 PM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,3 +1,13 @@
 file_name=str(input(">"))
 if ".gif" in file_name:
-    print()".jpg"".jpeg"".png"".pdf"".txt"".zip"
+    print('image/gif')
+elif ".jpg" or ".jpeg" in file_name:
+    print('image/jpeg')
+elif ".png" in file_name:
+    print('image/png')
+elif ".pdf" in file_name:
+    print('PDF')
+elif ".txt" in file_name:
+    print('text')
+elif ".zip" in file_name:
+    print('ZIP')
