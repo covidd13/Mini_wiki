8b4270c5f565b90c9b20d576d24c5cac66eddedc
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-10 09:21:39 +0000

    Wed, May 10, 2023, 2:51 PM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,15 +1,15 @@
-file_name=str(input(">"))
-if ".gif" in file_name:
+file_name.casefold()=str(input(">"))
+if ".gif" in file_name.casefold():
     print('image/gif')
-elif ".jpg" or ".jpeg" in file_name:
+elif ".jpg" or ".jpeg" in file_name.casefold():
     print('image/jpeg')
-elif ".png" in file_name:
+elif ".png" in file_name.casefold():
     print('image/png')
-elif ".pdf" in file_name:
+elif ".pdf" in file_name.casefold():
     print('PDF')
-elif ".txt" in file_name:
+elif ".txt" in file_name.casefold():
     print('text')
-elif ".zip" in file_name:
+elif ".zip" in file_name.casefold():
     print('ZIP')
 else:
     print('application/octet-stream')
