16381e6a8e0f73b214ea13c96c0f7e83e1221b7d
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-11 05:55:36 +0000

    Thu, May 11, 2023, 11:25 AM +05:30

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -2,10 +2,10 @@ x=int(input())
 y= input()
 z=int(input())
 if y=="+":
-    print(x+y)
+    print(x+z)
 if y=="-":
-    print(x-y)
+    print(x-z)
 if y=="*":
-    print(x*y)
+    print(x*z)
 if y=="/":
-    print(x/y)
+    print(x/z)
