7b87698a673ce1fb156106a3d942c11ada92dd50
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-11 19:32:18 +0000

    Fri, May 12, 2023, 1:02 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,14 @@
-def main():
-    
+def main(time):
+    if time in range(7,8):
+        print("Breakfast time")
+    elif time in range(12,13):
+        print("Lunch time")
+    elif time in range(18,19):
+        print("Dinner time")
 
 
 def convert(time):
-    ...
+    if time
 
 
 if __name__ == "__main__":
