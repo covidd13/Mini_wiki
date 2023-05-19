7e0baa71eb14b4baafe9618b6012858e5b13de81
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-19 07:56:45 +0000

    Fri, May 19, 2023, 1:26 PM +05:30

--- scraping_locally/scraping_locally.py
+++ scraping_locally/scraping_locally.py
@@ -2,8 +2,13 @@ from bs4 import BeautifulSoup
 from requests import get
 
 def main():
+    #lets first open file in read only mode
+    f=open('example.py','r')
+    s=f.read()
 
+    soup=BeautifulSoup(s,'html.parser')
 
+    print(soup)
 
 if __name__ == "__main__":
     main()
