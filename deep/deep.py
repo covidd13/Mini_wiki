fbab8f44d64679c8554846c85d8405d3a8940293
Raman Kukreti <ramankukreti00@gmail.com>
2023-05-20 02:48:00 +0530

    Delete deep directory

--- deep/deep.py
+++
@@ -1,6 +0,0 @@
-ans=str(input("What is the to the great question of life,the universe and everything?"))
-if (ans == "42" or ans.casefold()== "forty two" or ans.casefold()=="forty-two" ) or " " in ans:
-    ans.replace(" ","")
-    print("Yes")
-else:
-    print("No")
